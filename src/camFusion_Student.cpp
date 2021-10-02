e9b8d40dfd6f935ca6034614935c79762cdc43cd
shantam bajpai <51986101+bshantam97@users.noreply.github.com>
2021-10-02 16:21:40 -0400

    Completed Code for TTC Camera, Testing Now

--- src/camFusion_Student.cpp
+++ src/camFusion_Student.cpp
@@ -135,17 +135,94 @@ void show3DObjects(std::vector<BoundingBox> &boundingBoxes, cv::Size worldSize, 
 
 
 // associate a given bounding box with the keypoints it contains
+// Find all the keypoint matches belonging to each 3D object
+// For outlier rejection compute the lowest mean of all eucliedean distances and remove those that are too far away from the mean
+// I have the input as my current bounding box, previous keypoints, current keypoints and a vector of keypoint matches between current and previous frame
+// We need to update the std::vector<cv::KeyPoint> keypoints and std::vector<cv::DMatch> kptMatches
+
 void clusterKptMatchesWithROI(BoundingBox &boundingBox, std::vector<cv::KeyPoint> &kptsPrev, std::vector<cv::KeyPoint> &kptsCurr, std::vector<cv::DMatch> &kptMatches)
 {
-    // ...
+
+    // Loop over the current keypoints to find whether they are inside the region of interest or not
+    // previous frame is "queryIdx" and current frame is "trainIdx"
+    // Here what we have done is if my roi contains the keypoint I update the bounding box keypoints and also update bounding box 
+    // keypoint matches 
+ 
+    std::vector<cv::DMatch> KeypointMatch;
+    for (auto it = kptMatches.begin(); it != kptMatches.end(); it++) {
+        if (boundingBox.roi.contains(kptsCurr[it->trainIdx].pt)) KeypointMatch.push_back(*it);
+    }
+    // Euclidean distance computation
+    double meanEuclideanDistance = 0;
+
+    for (auto it = KeypointMatch.begin(); it != KeypointMatch.end(); it++) {
+        
+        meanEuclideanDistance+=std::sqrt(std::pow((kptsCurr[it->trainIdx].pt.x - kptsPrev[it->queryIdx].pt.x), 2)
+                                        + std::pow((kptsCurr[it->trainIdx].pt.y - kptsPrev[it->queryIdx].pt.y), 2));    
+    }
+    meanEuclideanDistance = meanEuclideanDistance / KeypointMatch.size();
+    // Now we filter the matches
+    for (auto it = KeypointMatch.begin(); it != KeypointMatch.end(); it++) {
+        double euclideanDistance = std::sqrt(std::pow((kptsCurr[it->trainIdx].pt.x - kptsPrev[it->queryIdx].pt.x), 2)
+                                            + std::pow((kptsCurr[it->trainIdx].pt.y - kptsPrev[it->queryIdx].pt.y), 2));
+        if (euclideanDistance < meanEuclideanDistance) {
+            boundingBox.keypoints.push_back(kptsCurr[it->trainIdx]);
+            boundingBox.kptMatches.push_back(*it);
+        }  
+    }
 }
 
 
 // Compute time-to-collision (TTC) based on keypoint correspondences in successive images
+// projecting object into camera: h0=f*(H/d0) and h1=f*(H/d1).Here f is the focal length, H is car height and d is the distance at t0 and t1
+// Relation to projection and distance: h1/h0 = (f*(H/d1))/(f*(H/d0)) which gives us d0 = d1*(h1/h0)
+// Substitution in Constant velocity model d1 = d0 - v0*(delta_t) = -v0*(delta_t) / (1 - (h1/h0))
+// TTC = d1/v0 = -(delta_t)/(1-(h1/h0))
+
 void computeTTCCamera(std::vector<cv::KeyPoint> &kptsPrev, std::vector<cv::KeyPoint> &kptsCurr, 
                       std::vector<cv::DMatch> kptMatches, double frameRate, double &TTC, cv::Mat *visImg)
 {
-    // ...
+    // Compute the distance ratios between all matched keypoints
+    std::vector<double> distRatios;
+    double dt = frameRate / 100.0;
+    for (auto it1 = kptMatches.begin(); it1 != kptMatches.end(); ++it1) {
+        // Obtain the current keypoint and its matched keypoint in the previous frame 
+        cv::KeyPoint kpOuterCurr = kptsCurr.at(it1->trainIdx);
+        cv::KeyPoint kpOuterPrev = kptsCurr.at(it1->queryIdx);
+
+        for (auto it2 = kptMatches.begin(); it2 != kptMatches.end(); ++it2) {
+            double minDist = 100.0;
+
+            cv::KeyPoint kpInnerCurr = kptsCurr.at(it2->trainIdx);
+            cv::KeyPoint kpInnerPrev = kptsPrev.at(it2->queryIdx);
+
+            // The idea here being for both the current and previous frame calculate the distance between each keypoints
+            // Then take their ratio's and push it back into the distRatios vector
+            double distCurr = cv::norm(kpOuterCurr.pt - kpInnerCurr.pt);
+            double distPrev = cv::norm(kpOuterPrev.pt - kpInnerPrev.pt);
+
+            // std::numeric_limits provides a way to query properties of arithmetic types e.g. the largest value for int is std::numeric_limits<int>::max()
+            // std::numeric_limits<double>::epsilon returns DBL_EPSILON with a possible value of 2.2 * e-16. So basically this condition is to avoid 
+            // dividing by ZERO. The other condition might be to take into consideration the car width/ diagonal length to constraint the keypoint distances
+            // on the car
+            if (distPrev > std::numeric_limits<double>::epsilon() && distCurr >= minDist) {
+                double distRatio = distCurr / distPrev;
+                distRatios.push_back(distRatio);
+            }
+        }
+    }
+
+    // Continue only if the distRatio list is not empty
+    if (distRatios.size() == 0) {
+        TTC = NAN;
+        return;
+    }
+
+    // Compute the camera based TTC from the distance ratios. Idea is to observe the scale change
+    double medianDistanceRatio;
+    std::sort(distRatios.begin(), distRatios.end());
+    medianDistanceRatio = (distRatios.size() % 2 == 0) ? (distRatios[(distRatios.size() / 2) - 1] + distRatios[distRatios.size()/2])/2.0 : distRatios[std::floor(distRatios.size() / 2)];
+    TTC = -dt / (1-medianDistanceRatio);
 }
 
 
@@ -174,8 +251,8 @@ void computeTTCLidar(std::vector<LidarPoint> &lidarPointsPrev,
     double MedianCurr = 0;
 
     // Compute the median for both the lidar point vectors and use that in the TTC Computation
-    MedianPrev = (prevXcoord.size() % 2 == 0) ? prevXcoord[prevXcoord.size() / 2] : prevXcoord[std::floor(prevXcoord.size() / 2)];
-    MedianCurr = (currXcoord.size() % 2 == 0) ? currXcoord[currXcoord.size() / 2] : currXcoord[std::floor(currXcoord.size() / 2)];
+    MedianPrev = (prevXcoord.size() % 2 == 0) ? (prevXcoord[(prevXcoord.size() / 2) - 1] + prevXcoord[(prevXcoord.size() / 2)]) / 2.0 : prevXcoord[std::floor(prevXcoord.size() / 2)];
+    MedianCurr = (currXcoord.size() % 2 == 0) ? (currXcoord[(currXcoord.size() / 2) - 1] + currXcoord[(currXcoord.size() / 2)]) / 2.0 : currXcoord[std::floor(currXcoord.size() / 2)];
 
     TTC = MedianCurr * dt / (MedianPrev - MedianCurr);
 }
@@ -221,8 +298,10 @@ void matchBoundingBoxes(std::vector<cv::DMatch> &matches, std::map<int, int> &bb
     // After we input the information we can iterate and compute the difference between number of keypoints and the one with 
     // the minimum difference will be input into the map
     // At most the time complexity should be O(numBoundingBox*maxKeypoints)
-    std::map<int,int> prevFrameKeypoints;
-    std::map<int,int> currFrameKeypoint;
+
+    std::map<int,int> prevFrameKeypoints, currFrameKeypoint;
+
+    // Store the boxId in the current frame and its associated number of keypoints
     std::pair<int,int> difference;
 
     // Store the boxId's and associated number of keypoints in the map
